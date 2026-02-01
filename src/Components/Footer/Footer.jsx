66400e1cfe154dbee666ff1193a6e8a01ff744af
sagnik mondal <sagnikmondal011@gmail.com>
2026-02-02 01:49:31 +0530

    modify

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,13 +11,13 @@ const Footer = () => {
   };
 
   return (
-    <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
+    <footer className="text-white py-8 px-[5vw] md:px-[7vw] lg:px-[20vw]">
       <div className="container mx-auto text-center">
         {/* Name / Logo */}
         <h2 className="text-xl font-semibold text-purple-500">Sagnik Mondal</h2>
 
         {/* Navigation Links - Responsive */}
-        <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
+        <nav className="flex flex-wrap justify-center space-x-2 sm:space-x-6 mt-4">
           {[
             { name: "About", id: "about" },
             { name: "Skills", id: "skills" },
@@ -36,7 +36,7 @@ const Footer = () => {
         </nav>
 
         {/* Social Media Icons - Responsive */}
-        <div className="flex flex-wrap justify-center space-x-4 mt-6">
+        <div className="flex flex-wrap justify-center space-x-4 mt-2">
           {[
             { icon: <FaFacebook />, link: "https://www.facebook.com/share/1C9btKtb21/" },
             { icon: <FaTwitter />, link: "https://x.com/SagnikM33675590" },
